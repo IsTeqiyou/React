92eb449713850ac066736051ed81326091b8a46c
Taqiyuddin <160695552+IsTeqiyou@users.noreply.github.com>
2024-11-12 10:39:20 +0700

    readme

---
+++ testing/src/m2/file.jsx
@@ -0,0 +1,11 @@
+function App() {
+    return (
+        <>
+        <img src="WhatsApp Image 2024-11-04 at 11.32.39 (1).jpg" alt="cobareact" ></img>
+        <h1> WELCOME </h1>
+        <p>Lorem ipsum dolor sit amet consectetur.</p>
+        </>
+    )
+}
+
+export default App;
